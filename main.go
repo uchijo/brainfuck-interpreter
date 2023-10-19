43b8dfa0bd57b4ab17d258fc64dc8531f682da1d
Uchijo <kgtnttki.yuta@gmail.com>
2023-10-19 22:42:57 +0900

    引数で渡せる

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/uchijo/brainfuck-interpreter/machine"
 )
 
+type cmdArgs struct {
+	input string
+}
+
 func main() {
+	flag.Parse()
+	rawArgs := flag.Args()
+	args := cmdArgs{
+		input: rawArgs[0],
+	}
+
 	engine := machine.NewEngine(
-		"+++++++++[>++++++++>+++++++++++>+++>+<<<<-]>.>++.+++++++..+++.>+++++.<<+++++++++++++++.>.+++.------.--------.>+.>+.",
+		args.input,
 		1000,
 	)
 	if err := engine.Eval(); err != nil {
